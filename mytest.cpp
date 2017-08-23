2c9f92ea044384c3c7ab80b378e21c606f1212a9
gjones1911 <gjones2@vols.utk.edu>
2017-08-23 19:52:40 -0400

    Here We Go!!

---
+++ mytest.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+	int num = 0; 
+	string bob;
+
+	char * name;
+
+	printf("Give me your name sir.....\n");
+
+	cin>>bob;
+
+	printf("Thanks %s!!", bob);
+
+	return 0;
+}
