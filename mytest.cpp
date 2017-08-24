129ebfdf2526d31a70ce6abd4d4b32d11b02fb16
gjones1911 <gjones2@vols.utk.edu>
2017-08-24 15:21:18 -0400

    Update mytest.cpp

--- mytest.cpp
+++ mytest.cpp
@@ -7,13 +7,23 @@ using namespace std;
 int main(int argc, char ** argv)
 {
 	int num = 0; 
-	string bob, first, last;
-
-	char * name;
+	string name = "", first, last;
 
 	printf("Give me your first and last name.....\n");
 
 	cin>>first>>last;
+	
+	while(cin>>name)
+	{
+		if( num == 0)
+		{
+			first = name;
+		}
+		else if( num == 1)
+		{
+			last = name;	
+		}
+	}
 
 	printf("Thanks %s %s!!\n", first.c_str(), last.c_str() );
 
